0b866b3a3dcc09c6f00dbc1644fcc695ef1bc236
kim <wlaud604@naver.com>
2020-11-19 23:47:37 +0900

    create 3 packages

---
+++ algorithm/script/custom_subscriber.py
@@ -0,0 +1,9 @@
+import rospy
+from common_msgs import TimePose
+
+def callback(msg):
+    print "subscribe:", msg.timestamp.secs%100, msg.vect.x, msg.vect.y, msg.vect.z
+
+rospy.init_node('custom_subscriber')
+sub = rospy.Subscriber('custom_msg', TimePose, callback)
+rospy.spin()
---
+++ sensor/script/custom_publisher.py
@@ -0,0 +1,15 @@
+import rospy
+from geometry_msgs.msg import Vector3
+from common_msgs import TimePose
+
+rospy.init_node('custom_publisher')
+pub = rospy.Publisher('custom_msg', TimePose, queue_size=1)
+msg = TimePose()
+rate = rospy.Rate(1)
+while not rospy.is_shutdown():
+    msg.timestamp = rospy.get_rostime()
+    second = msg.timestamp.secs
+    msg.vect = Vector3(x=second%4, y=second%7, z=second%5)
+    pub.publish(msg)
+    print "publish:", msg.timestamp.secs%100, msg.vect.x, msg.vect.y, msg.vect.z
+    rate.sleep()
