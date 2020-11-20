7aa67140edbee7402e193bde9ecec3b6fedc8173
kim <wlaud604@naver.com>
2020-11-20 19:54:42 +0900

    message file

--- sensor/script/custom_publisher.py
+++ sensor/script/custom_publisher.py
@@ -9,9 +9,9 @@ rate = rospy.Rate(1)
 while not rospy.is_shutdown():
     msg.timestamp = rospy.get_rostime()
     second = msg.timestamp.secs
-    msg.vect = Vector3(x=second%4, y=second%7, z=second%5)
+    msg.pose = Vector3(x=second%4, y=second%7, z=second%5)
     pub.publish(msg)
-    print "publish:", msg.timestamp.secs%100, msg.vect.x, msg.vect.y, msg.vect.z
+    print "publish:", msg.timestamp.secs%100, msg.pose.x, msg.pose.y, msg.pose.z
     rate.sleep()
 
 
