b5293e9bd7004f2ea494bf9bada51e2290ae5bee
kim <wlaud604@naver.com>
2020-11-20 21:34:02 +0900

    message file

--- sensor/script/custom_publisher.py
+++ sensor/script/custom_publisher.py
@@ -10,9 +10,11 @@ rate = rospy.Rate(1)
 while not rospy.is_shutdown():
     msg.timestamp = rospy.get_rostime()
     second = msg.timestamp.secs
-    msg.pose = Vector3(x=second%2, y=second%4, z=second%6)
+    msg.pose = Vector3(x=second.radint(1,100), y=second.randint(1,100), z=0)
+    msg.pose.z = msg.pose.x * msg.pose.y 
     pub.publish(msg)
-    print "publish:", msg.timestamp.secs%100, msg.pose.x, msg.pose.y, msg.pose.z
+    print("X * Y = Z ")
+    print ("publish:", msg.timestamp.secs%100, msg.pose.x, msg.pose.y, msg.pose.z)
     rate.sleep()
 
 
