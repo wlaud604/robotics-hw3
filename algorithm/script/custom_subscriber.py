87e0e7794764f4edf1a124e9dc2fd7a6b46ac7f7
kim <wlaud604@naver.com>
2020-11-20 19:54:19 +0900

    message file

--- algorithm/script/custom_subscriber.py
+++ algorithm/script/custom_subscriber.py
@@ -2,7 +2,7 @@ import rospy
 from common_msgs.msg import TimePose
 
 def callback(msg):
-    print "subscribe:", msg.timestamp.secs%100, msg.vect.x, msg.vect.y, msg.vect.z
+    print "subscribe:", msg.timestamp.secs%100, msg.pose.x, msg.pose.y, msg.pose.z
 
 rospy.init_node('custom_subscriber')
 sub = rospy.Subscriber('custom_msg', TimePose, callback)
